cf0b8a6037936b6bdd9282efa7a1be37cea7e529
alexy05081993-lang <alexy05081993@gmail.com>
2026-05-01 18:43:15 +0300

    Delete lib directory

--- lib/db/src/schema/events.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
-export const eventsTable = pgTable("events", {
-  id: serial("id").primaryKey(),
-  eventType: text("event_type").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
-
-export type Event = typeof eventsTable.$inferSelect;
